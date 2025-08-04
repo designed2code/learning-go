32af5e8d1e8f1e832fc68165ccfc9d426bbc696e
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-04 16:52:14 +0530

    understading functions

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,11 +9,11 @@ func main(){
 	var investmentAmount float64
 	  expectedReturnRate := 5.5
 	 var years float64
-	 fmt.Print("Investment Value: ")
+	 outputText("Investment Value: ")
 	 fmt.Scan(&investmentAmount)
-	 fmt.Print("Expected Return Rate: ")
+	 outputText("Expected Return Rate: ")
 	 fmt.Scan(&expectedReturnRate)
-	 fmt.Print("Years: ")
+	 outputText("Years: ")
 	 fmt.Scan(&years)
 	 futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100,(years))
 	 futureRealValue := futureValue/math.Pow(1+inflationRate/100,years)
@@ -22,4 +22,8 @@ func main(){
 	// fmt.Println("Future Value",futureValue)
 	fmt.Print(formattedFV)
 	fmt.Println("Future Value Adjusted for inflation",futureRealValue)
+}
+
+func outputText(text string){
+	fmt.Print(text)
 }
