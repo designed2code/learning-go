d76ea246e35c1ffcdfc61435c847423ca542cc85
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-04 17:34:01 +0530

    making code modular by using functions

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -3,22 +3,39 @@ package main
 import "fmt"
 
 func main(){
-	var revenue float64 
-	var expenses float64
-	var taxRate float64
-	fmt.Print("Revenue: ") 
-	fmt.Scan(&revenue)
+	// var revenue float64 
+	// var expenses float64
+	// var taxRate float64
+	revenue := getInputValues("Revenue: ")
+	// fmt.Print("Revenue: ") 
+	// fmt.Scan(&revenue)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	expenses := getInputValues("Expenses: ")
+	// fmt.Print("Expenses: ")
+	// fmt.Scan(&expenses)
 
-	fmt.Print("TaxRate: ")
-	fmt.Scan(&taxRate)
+	taxRate := getInputValues("TaxRate: ")
+	// fmt.Print("TaxRate: ")
+	// fmt.Scan(&taxRate)
 
-	ebt := revenue - expenses 
-	profit := ebt * (1-taxRate/100)
-	ratio := ebt/profit
+	
+	ebt,profit,ratio := calculateValues(revenue,expenses,taxRate)
+	
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+}
+
+func getInputValues(text string) (float64){
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateValues(revenue float64,expenses float64,taxRate float64) (float64,float64,float64){
+	ebt := revenue - expenses 
+	profit := ebt * (1-taxRate/100)
+	ratio := ebt/profit
+	return ebt,profit,ratio
 }
