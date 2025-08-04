f055a5efe940d09a4f9b9ba4bd05f79f776e778a
Sachin <sachin@Sachins-MacBook-Air.local>
2025-08-04 15:38:35 +0530

    using the const keyword for variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,9 +5,12 @@ import (
 	"math"
 )
 func main(){
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	 expectedReturnRate := 5.5 
 	 years  := 10.0
 	 futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100,(years))
+	 futureRealValue := futureValue/math.Pow(1+inflationRate/100,years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
